ac9900abf04f1d80765c0c45d27571e1e0332232
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2025-09-01 20:15:27 +0530

    Init

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { type Message, LangChainAdapter } from 'ai';
+import { createReactAgent } from '@langchain/langgraph/prebuilt';
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { SystemMessage } from '@langchain/core/messages';
+import { convertVercelMessageToLangChainMessage } from '@/utils/message-converters';
+import { logToolCallsInDevelopment } from '@/utils/stream-logging';
+
+const AGENT_SYSTEM_TEMPLATE = `You are a personal assistant named Assistant0. You are a helpful assistant that can answer questions and help with tasks. You have access to a set of tools, use the tools as needed to answer the user's question.`;
+
+/**
+ * This handler initializes and calls an tool calling ReAct agent.
+ * See the docs for more information:
+ *
+ * https://langchain-ai.github.io/langgraphjs/tutorials/quickstart/
+ */
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    /**
+     * We represent intermediate steps as system messages for display purposes,
+     * but don't want them in the chat history.
+     */
+    const messages = (body.messages ?? [])
+      .filter((message: Message) => message.role === 'user' || message.role === 'assistant')
+      .map(convertVercelMessageToLangChainMessage);
+
+    const llm = new ChatGoogleGenerativeAI({
+      model: "gemini-2.5-flash",
+      temperature: 0,
+      apiKey: process.env.GOOGLE_API_KEY,
+    });
+
+    /**
+     * Use a prebuilt LangGraph agent.
+     */
+    const agent = createReactAgent({
+      llm,
+      tools: [],
+      /**
+       * Modify the stock prompt in the prebuilt agent. See docs
+       * for how to customize your agent:
+       *
+       * https://langchain-ai.github.io/langgraphjs/tutorials/quickstart/
+       */
+      messageModifier: new SystemMessage(AGENT_SYSTEM_TEMPLATE),
+    });
+
+    /**
+     * Stream back all generated tokens and steps from their runs.
+     *
+     * See: https://langchain-ai.github.io/langgraphjs/how-tos/stream-tokens/
+     */
+    const eventStream = agent.streamEvents({ messages }, { version: 'v2' });
+
+    // Log tool calling data. Only in development mode
+    const transformedStream = logToolCallsInDevelopment(eventStream);
+
+    return LangChainAdapter.toDataStreamResponse(transformedStream);
+  } catch (e: any) {
+    console.error(e);
+    return NextResponse.json({ error: e.message }, { status: e.status ?? 500 });
+  }
+}
